970f9893cf1bb969b5cd76bb977636b322f32b82
GYM-PUMP <nnrda.nnrda@gmail.com>
2020-01-11 05:57:32 +0000

    [start&finish] 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '042e46241eb4e0542a6a4c6d7dc896afaa55051037c7589485399c9f3e5305d6ceda7b24c7cbef6712239c7cbdc3cb1e74fe1d44caccea74927850764d0f60ec'
